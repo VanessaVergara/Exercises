19ee79ec737b0c8ba5d696ce86e2ddda216332fd
VanessaVergara <75291123+VanessaVergara@users.noreply.github.com>
2021-09-07 14:23:09 -0400

    arrow functions exercise

---
+++ Arrow Functions Exercise/index.js
@@ -0,0 +1,16 @@
+//ES5 Map Callback
+// function double(arr) {
+//   return arr.map(function(val) {
+//     return val * 2;
+//   });
+// }
+
+//ES2015 Arrow Functions Shorthand
+
+const double = arr => 
+ arr.map((val) => val * 2);
+
+
+
+const squareAndFindEvens = (numbers) => numbers.map((num)=> num **2).filter((square) => square % 2 === 0);
+    
